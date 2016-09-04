d87b6fb386b2a1ad9953ade6bf59c3678e8c3cb6
Petitsurume <gamemannew2013@yahoo.co.jp>
2016-09-04 19:17:58 +0900

    ア

--- src/config.py
+++ src/config.py
@@ -7,4 +7,5 @@ if(not os.path.exists(config_dir+os.sep+"api.json")):
     print("APIのコンフィグ(config/api.json)が存在しません。APIのweb_config.jsonをconfig/api.jsonにコピーしてください。")
     sys.exit(1)
 # Web特有コンフィグがあるか確認
-if(os.path.exists(config_dir+os.sep+"web.json"))
+if(not os.path.exists(config_dir+os.sep+"web.json")):
+    print("設定ファイルが存在しません。対話モードで設定ファイルを作成します！")
