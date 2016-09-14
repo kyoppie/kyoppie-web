322562aa1b2a1d4fc2ff26e54d0645443c27cec3
Petitsurume <gamemannew2013@yahoo.co.jp>
2016-09-14 12:36:32 +0900

    config周りを変更

--- src/config.py
+++ src/config.py
@@ -14,26 +14,72 @@ else:
     f = open(config_dir+os.sep+"api.json","r")
     api = json.load(f)
     f.close()
-# Web特有コンフィグがあるか確認
+# Web特有コンフィグがあるか確認(更新される可能性もあるので)
+configSchema = [
+    {
+        "name":"url",
+        "text":"Webが動作するURLを入力してください (例:https://example.com ) > ",
+        "type":"text"
+    },
+    {
+        "name":"api",
+        "text":"APIが動作しているURLを入力してください (例: https://api.example.com ) > ",
+        "type":"text"
+    },
+    {
+        "name":"port",
+        "text":"Webが動作するポートを入力してください (例:8080) > ",
+        "type":"int"
+    },
+    {
+        "name":"is_debug",
+        "text":"開発用機能を有効にしますか\n(インターネット上に公開される環境では有効にしないでください) (Y/N) > ",
+        "type":"yorn"
+    }
+]
+questionFlag = False
 if(not os.path.exists(config_dir+os.sep+"web.json")):
     print("設定ファイルが存在しません。対話モードで設定ファイルを作成します！")
     print("※URLを入力する場合、最後に/はいりません！")
-    # 欲しいものは
-    # - WebのURL
-    # - APIのURL
-    # - Webの稼働ポート
-    WebUrl = input("Webが動作するURLを入力してください (例:https://example.com ) > ")
-    ApiUrl = input("APIが動作しているURLを入力してください (例: https://api.example.com ) > ")
-    WebPort = input("Webが動作するポートを入力してください (例:8080) > ")
-    web={
-        "url":WebUrl,
-        "port":int(WebPort),
-        "api":ApiUrl
-    }
+    for question in configSchema:
+        print(question)
+    web={}
     f = open(config_dir+os.sep+"web.json","w")
     json.dump(web,f)
     f.close()
 else:
     f = open(config_dir+os.sep+"web.json","r")
     web = json.load(f)
+    f.close()
+    questionFlag = True
+for question in configSchema:
+    if(web.get(question["name"]) == None):
+        if(questionFlag):
+            print("kyoppieのアップデートに伴い、いくつかの設定項目が追加されました。\nつきましては、お手数おかけしますが追加された設定項目に対話形式でお答えください。")
+            questionFlag = False
+        while(True):
+            print("")
+            _i = input(question["text"])
+            if(question["type"] == "text"):
+                web[question["name"]] = _i
+                break
+            elif(question["type"] == "yorn"):
+                if(_i == "y" or _i == "Y"):
+                    web[question["name"]] = True
+                    break
+                elif(_i == "n" or _i == "N"):
+                    web[question["name"]] = False
+                    break
+                else:
+                    print("不正な文字列が指定されました。")
+            elif(question["type"] == "int"):
+                try:
+                    web[question["name"]] = int(_i)
+                    break
+                except:
+                    print("不正な文字列が指定されました。")
+            else:
+                print("なんかおかしい")
+    f = open(config_dir+os.sep+"web.json","w")
+    json.dump(web,f)
     f.close()
