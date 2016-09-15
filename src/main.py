38c052cb3b274d9347b3a561d87d90973db213ba
Petitsurume <gamemannew2013@yahoo.co.jp>
2016-09-15 11:54:14 +0900

    hello,kyoppie!

--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
 import config
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
+app.template_folder = "views"
+app.jinja_env.add_extension("pyjade.ext.jinja.PyJadeExtension")
+app.jinja_env.auto_reload=config.web["is_debug"]
+app.debug=config.web["is_debug"]
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template("index.jade")
+
+if(__name__ == "__main__"):
+    app.run(
+        port=config.web["port"]
+    )
