fe79bd20f84804213ec4d0ae37825d042240264d
Petitsurume <gamemannew2013@yahoo.co.jp>
2016-09-18 18:30:42 +0900

    セッションにアクセストークンを保持するように

--- src/ajax.py
+++ src/ajax.py
@@ -1,9 +1,12 @@
-from flask import Blueprint,request,jsonify
+from flask import Blueprint,request,jsonify,session
 import api
 
 app = Blueprint(__name__,"ajax",url_prefix="/_/ajax")
 
 @app.route('/login',methods=["POST"])
 def login():
-    print(api.login(request.form["screenName"],request.form["password"]))
+    res = api.login(request.form["screenName"],request.form["password"])
+    if(res["result"] == False):
+        return jsonify(status="ng",message=res["error"])
+    session["access_token"] = res["response"]["token"]
     return jsonify(status="ok")
