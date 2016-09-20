40230d2b5208b4df44940946e73364626af80510
Petitsurume <gamemannew2013@yahoo.co.jp>
2016-09-20 19:35:50 +0900

    staticのディレクトリわけ

---
+++ src/static/js/api.js
@@ -0,0 +1,23 @@
+$.extend({
+    api:{
+        get_access_token:function(){
+            return $("meta[name=access_token]").attr("content");
+        },
+        request:function(method,endpoint,params){
+            return $.ajax({
+                url:CONFIG.api+"/"+endpoint,
+                type:method,
+                data:params,
+                headers:{
+                    "X-Kyoppie-Access-Token":$.api.get_access_token()
+                }
+            })
+        },
+        get:function(endpoint,params){
+            return $.api.request("GET",endpoint,params);
+        },
+        post:function(endpoint,params){
+            return $.api.request("POST",endpoint,params);
+        }
+    }
+})
