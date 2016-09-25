55605d1ee7e52617974f63326c5761d6e4d8baad
Petitsurume <gamemannew2013@yahoo.co.jp>
2016-09-25 18:55:52 +0900

    うるさいぞ

--- src/tojinja.py
+++ src/tojinja.py
@@ -1,6 +1,7 @@
 from flask import Blueprint,request,jsonify,session,Response
 import pyjade.utils
 import pyjade.ext.jinja
+import config
 app = Blueprint(__name__,"tojinja",url_prefix="/_/tojinja")
 
 def tojinja(name):
@@ -15,9 +16,14 @@ def tojinja(name):
     html=html.replace("\r","\\r")
     html=html.replace("\n","\\n")
     html=html.replace("\"","\\\"")
-    print(html)
+    if(config.web["is_debug"]):
+        print(html)
     html = "function template_"+name+"(locals){var template=\""+html+"\";return nunjucks.renderString(template,locals)}"
     return Response(html,mimetype="text/javascript")
-@app.route('/post')
-def pf():
-    return tojinja("post")
+templates = [
+    "post",
+]
+for template_name in templates:
+    @app.route('/'+template_name)
+    def pf():
+        return tojinja(template_name)
