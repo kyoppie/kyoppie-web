a42a349c9e4a7e0f1a81baaf9872ab9812c3ec28
Petitsurume <gamemannew2013@yahoo.co.jp>
2016-09-26 18:50:29 +0900

    とりあえず日付表示を実装

---
+++ src/static/js/relative-time.js
@@ -0,0 +1,32 @@
+$(function(){
+    setInterval(function(){
+        var now_time = Date.now()/1000;
+        var offset = -(new Date().getTimezoneOffset()*60);
+        var f = Math.floor;
+        var ssf = function(num){
+            return num < 10 ? "0"+f(num) : f(num);
+        }
+        now_time += offset;
+        $(".relative_time").each(function(){
+            var target_time = new Date(this.getAttribute("datetime")).getTime()/1000;
+            target_time += offset;
+            var sabun = f(now_time-target_time);
+            var text = "";
+            if(sabun < 120) {
+                text = sabun+"秒前";
+            } else if(sabun < 60*60) {
+                text = f(sabun/60)+"分前";
+            } else if(sabun < 120*60) {
+                text = f(sabun/60/60)+"時間と"+f(sabun/60)+"分前";
+            } else if(sabun < 24*60*60) {
+                text = "今日の"+ssf(target_time/60/60)+":"+ssf(target_time/60%60)
+            } else if(sabun < 30*24*60*60) {
+                text = f(sabun / (24*60*60))+"日前の"+ssf(target_time/60/60%24)+":"+ssf(target_time/60%60)
+            } else {
+                text = "むっちゃ前";
+            }
+            if(this.innerText != text) this.innerText = text;
+            console.log(sabun)
+        })
+    },1000)
+})
