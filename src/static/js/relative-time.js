346cc29617a705b471636d0586c11f1160e7da2e
Petitsurume <gamemannew2013@yahoo.co.jp>
2016-09-26 19:36:49 +0900

    fix?

--- src/static/js/relative-time.js
+++ src/static/js/relative-time.js
@@ -1,11 +1,11 @@
 $(function(){
+    var f = Math.floor;
+    var ssf = function(num){
+        return num < 10 ? "0"+f(num) : f(num);
+    }
     setInterval(function(){
-        var now_time = Date.now()/1000;
+        var now_time = f(Date.now()/1000);
         var offset = -(new Date().getTimezoneOffset()*60);
-        var f = Math.floor;
-        var ssf = function(num){
-            return num < 10 ? "0"+f(num) : f(num);
-        }
         now_time += offset;
         $(".relative_time").each(function(){
             var target_time = new Date(this.getAttribute("datetime")).getTime()/1000;
@@ -18,10 +18,10 @@ $(function(){
                 text = f(sabun/60)+"分前";
             } else if(sabun < 120*60) {
                 text = f(sabun/60/60)+"時間と"+f(sabun/60)+"分前";
-            } else if(sabun < 24*60*60) {
+            } else if(f(target_time/60/60/24) == f(now_time/60/60/24)) {
                 text = "今日の"+ssf(target_time/60/60%24)+":"+ssf(target_time/60%60)
             } else if(sabun < 30*24*60*60) {
-                text = f(sabun / (24*60*60))+"日前の"+ssf(target_time/60/60%24)+":"+ssf(target_time/60%60)
+                text = (f(now_time/60/60/24) - f(target_time/60/60/24))+"日前の"+ssf(target_time/60/60%24)+":"+ssf(target_time/60%60)
             } else {
                 text = "むっちゃ前";
             }
