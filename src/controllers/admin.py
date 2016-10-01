c049f09b890fc363d26023f003ce2fbbdc1f083c
Petitsurume <gamemannew2013@yahoo.co.jp>
2016-10-01 18:43:15 +0900

    できた

--- src/controllers/admin.py
+++ src/controllers/admin.py
@@ -7,5 +7,17 @@ app = Blueprint(__name__,"admin",url_prefix="/admin")
 @app.route('/')
 @utils.login_required
 def adminIndex():
-    res = api.get("account/show",login=True)
+    res = api.get("account/show",login=True)["response"]
     return render_template("admin/index.jade",user=res)
+
+@app.route('/file_servers')
+@utils.login_required
+def fileserverIndex():
+    res = api.get("admin/file_servers/list",login=True)["response"]
+    return render_template("admin/file_servers/index.jade",servers = res)
+
+@app.route('/file_servers/<id>')
+@utils.login_required
+def fileserverShow(id):
+    res = api.get("admin/file_servers/show",{"id":id},login=True)["response"]
+    return render_template("admin/file_servers/show.jade",server = res)
