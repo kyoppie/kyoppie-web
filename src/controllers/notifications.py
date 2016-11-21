c0a73efa8848ca071f9043a816df12d6e25eddf1
Petitsurume <gamemannew2013@yahoo.co.jp>
2016-11-21 19:49:24 +0900

    通知周りがログイン必須になっていなかった不具合を修正

--- src/controllers/notifications.py
+++ src/controllers/notifications.py
@@ -1,17 +1,23 @@
 from flask import Blueprint,render_template,redirect
 import api
+import utils
 app = Blueprint(__name__,"notifications",url_prefix="/notifications")
 
 @app.route('/')
+@utils.login_required
 def notificationList():
     res = api.get("notifications/list",login=True)["response"]
     return render_template("notifications/index.jade",notifications = res)
+
 @app.route('/<id>/user')
+@utils.login_required
 def notificationUser(id):
     res = api.get("notifications/show",{"id":id},login=True)["response"]
     res_ = api.post("notifications/read",{"id":id},login=True)["response"]
     return redirect("/u/"+res["targetUser"]["screenName"])
+
 @app.route('/<id>/post')
+@utils.login_required
 def notificationPost(id):
     res = api.get("notifications/show",{"id":id},login=True)["response"]
     res_ = api.post("notifications/read",{"id":id},login=True)["response"]
