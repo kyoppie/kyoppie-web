945641830c387755f1519be69e525ac805f22c4e
petitsurume <gamemannew2013@yahoo.co.jp>
2017-02-03 13:06:58 +0900

    wait

--- .ci-files/run.sh
+++ .ci-files/run.sh
@@ -8,9 +8,15 @@ npm install
 npm run migrate
 npm install -g forever
 forever start -c "node --harmony" src/main.js
+until curl localhost:4005 > /dev/null 2> /dev/null do
+    sleep 1
+done
 cd ..
 cp kyoppie-api/web_config.json config/api.json
 cp .ci-files/web.json config/
 pip3 install flask pyjade requests
 cd src
 forever start -c "python3" main.py
+until curl localhost:4006 > /dev/null 2> /dev/null do
+    sleep 1
+done
