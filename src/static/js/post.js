fa458a51f0bab3d06bafc21aa8b8e1219a26cfee
petitsurume <gamemannew2013@yahoo.co.jp>
2017-02-18 00:53:52 +0900

    ふぁぼ/RPした時にカウントを増やす

--- src/static/js/post.js
+++ src/static/js/post.js
@@ -1,16 +1,28 @@
 $(function(){
     $(document).on("click",".post-favorite",function(){
-        if($(this).hasClass("post-favorited")) return;
-        $.api.post("posts/favorite",{id:$(this).data("post-id")})
-        $(this).addClass("post-favorited")
-        $(this).children("i.fa").removeClass("fa-star-o").addClass("fa-star")
-        $(this).next().text((parseInt($(this).next().text())||0)+1);
+        if($(this).hasClass("post-favorited")) return
+        $.api.post("posts/favorite",{id:$(this).data("post-id")}).then(function(res){
+            if(res.result){
+                $this.next().text(res.result.favoriteCount)
+            }
+        })
+        $this.addClass("post-favorited")
+        $this.children("i.fa").removeClass("fa-star-o").addClass("fa-star")
     })
     $(document).on("click",".post-repost",function(){
+        var $this = $(this)
+        if($this.hasClass("post-reposted")) return
         if(!confirm("この投稿をRePostしますか?")) return
-        $.api.post("posts/repost",{id:$(this).data("post-id")})
-        $(this).addClass("post-reposted")
-        $(this).next().text((parseInt($(this).next().text())||0)+1);
+        $.api.post("posts/repost",{id:$this.data("post-id")}).then(function(res){
+            if(res.result){
+                // TODO
+                // $this.next().text(res.response.repostTo.repostCount)
+                $.api.get("posts/show",{id:$this.data("post-id")}).then(function(res){
+                    $this.next().text(res.response.repostCount);
+                })
+            }
+        })
+        $this.addClass("post-reposted")
     })
     $(document).on("click",".post-reply",function(){
         var message = prompt("reply message","@"+$(this).parents(".post").data("user-screen-name")+" ")
